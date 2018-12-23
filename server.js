1a94b28cadf374af81d9ee42cb7f27395e03de83
tarasenkoDm <dm.a.tarasenko@gmail.com>
2018-12-22 19:23:55 -0500

    fixed typo in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<hw-star-wars>'));
+app.use(express.static(__dirname + '/dist/hw-star-wars'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<hw-star-wars>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/hw-star-wars/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
